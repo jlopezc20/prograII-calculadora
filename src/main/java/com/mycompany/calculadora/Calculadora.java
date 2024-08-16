11785499722c08bf47f4faecab8a9f21cce71912
jlopezc20 <jlopezc20@miumg.edu.gt>
2024-08-15 19:31:21 -0600

    Inicial commit

---
+++ src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+import com.mycompany.calculadora.visual.Principal;
+
+/**
+ *
+ * @author jgr_1
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+        Principal p = new Principal();
+        p.setVisible(true);
+    }
+}
